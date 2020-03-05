fb0bf0babc19678adcbaf324c1f7ba96a991b2a3
lilifeng <lilifeng-02@163.com>
2020-03-05 11:34:15 +0800

    first commit

---
+++ scripts/test_hello.py
@@ -0,0 +1,19 @@
+import allure
+
+
+class TestHello:
+
+    def test_hello1(self):
+
+       assert 1
+
+
+
+    def test_hello2(self):
+
+
+        assert 0
+
+    def test_hello3(self):
+
+        assert 1
